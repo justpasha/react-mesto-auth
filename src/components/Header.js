e6cdbae6a797792ea5aaea3bf8a6d44f2e0ec67d
Pavel Drobyshev <pavel.drob01@gmail.com>
2021-05-14 15:40:40 +0500

    feat: добавить меню

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation, useHistory } from 'react-router-dom';
 import logo from '../images/logo.svg';
 
@@ -9,6 +9,13 @@ function Header({ onLogout, email }) {
     path: '',
     text: 'Выйти',
   };
+  let isMainPage = false;
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  if (location.pathname === '/') {
+    isMainPage = true;
+  }
 
   if (location.pathname === '/signin') {
     data.path = '/signup';
@@ -20,26 +27,69 @@ function Header({ onLogout, email }) {
   }
 
   function handleClick() {
-    if (location.pathname === '/') {
+    if (isMainPage) {
       onLogout();
+      setIsMenuOpen(false);
     } else {
       history.push(data.path);
     }
   }
 
+  function handleOpenMenu() {
+    setIsMenuOpen(true);
+  }
+
+  function handleCloseMenu() {
+    setIsMenuOpen(false);
+  }
+
   return (
     <header className="header">
-      <img src={logo} alt="лого" className="header__logo" />
-      <div className="header__info-container">
-        <p className="header__text">{location.pathname === '/' ? email : ''}</p>
+      <div
+        className={`header__menu ${
+          isMenuOpen && isMainPage ? 'header__menu_opened' : ''
+        }`}
+      >
+        <p className="header__text">{isMainPage ? email : ''}</p>
         <button
           className={`header__button ${
-            location.pathname === '/' ? 'header__button_type_exit' : ''
+            isMainPage ? 'header__button_type_exit' : ''
           }`}
           onClick={handleClick}
         >
           {data.text}
         </button>
+      </div>
+
+      <div className="header__container">
+        <img src={logo} alt="лого" className="header__logo" />
+
+        {isMainPage ? (
+          <button
+            onClick={!isMenuOpen ? handleOpenMenu : handleCloseMenu}
+            className={`header__menu-button ${
+              isMenuOpen ? 'header__menu-button_type_close' : ''
+            }`}
+          ></button>
+        ) : (
+          ''
+        )}
+
+        <div
+          className={`header__info-container ${
+            isMainPage ? 'header__info-container_hidden' : ''
+          }`}
+        >
+          <p className="header__text">{isMainPage ? email : ''}</p>
+          <button
+            className={`header__button ${
+              isMainPage ? 'header__button_type_exit' : ''
+            }`}
+            onClick={handleClick}
+          >
+            {data.text}
+          </button>
+        </div>
       </div>
     </header>
   );
